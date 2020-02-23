9caefaa6d37c1aa5d341b3dbe082c25991b7b1d5
yohan <783b8c87@scimetis.net>
2020-02-23 15:17:38 +0100

    Moved cron tasks to another stack. Added a second fetchmail. Moved image building to startup script. Moved volume management to startup script.

--- start_or_update.sh
+++ start_or_update.sh
@@ -6,14 +6,23 @@ secrets/docker-mail-stack/fetchmailrc-scimetis
 sudo chown root. fetchmailrc-orange fetchmailrc-scimetis users 15-lda.conf feed2imaprc
 sudo chmod a-r feed2imaprc
 
-sudo chown root:root crontab
-sudo chmod 644 crontab
+unset VERSION_DOVECOT VERSION_FETCHMAIL VERSION_FEED2IMAP
+VERSION_DOVECOT=$(git ls-remote https://git.scimetis.net/yohan/docker-dovecot.git| head -1 | cut -f 1|cut -c -10)
+VERSION_FETCHMAIL=$(git ls-remote https://git.scimetis.net/yohan/docker-fetchmail.git| head -1 | cut -f 1|cut -c -10)
+VERSION_FEED2IMAP=$(git ls-remote https://git.scimetis.net/yohan/docker-feed2imap.git| head -1 | cut -f 1|cut -c -10)
 
-# --force-recreate is used to recreate container when crontab file has changed
-unset VERSION_DOVECOT VERSION_FETCHMAIL VERSION_FEED2IMAP VERSION_CRON
-VERSION_DOVECOT=$(git ls-remote https://git.scimetis.net/yohan/docker-dovecot.git| head -1 | cut -f 1|cut -c -10) \
-VERSION_FETCHMAIL=$(git ls-remote https://git.scimetis.net/yohan/docker-fetchmail.git| head -1 | cut -f 1|cut -c -10) \
-VERSION_FEED2IMAP=$(git ls-remote https://git.scimetis.net/yohan/docker-feed2imap.git| head -1 | cut -f 1|cut -c -10) \
-VERSION_CRON=$(git ls-remote https://git.scimetis.net/yohan/docker-cron.git| head -1 | cut -f 1|cut -c -10) \
+rm -rf ~/build
+mkdir -p ~/build
+git clone https://git.scimetis.net/yohan/docker-dovecot.git ~/build/docker-dovecot
+sudo docker build -t dovecot:$VERSION_DOVECOT ~/build/docker-dovecot
+git clone https://git.scimetis.net/yohan/docker-fetchmail.git ~/build/docker-fetchmail
+sudo docker build -t fetchmail:$VERSION_FETCHMAIL ~/build/docker-fetchmail
+git clone https://git.scimetis.net/yohan/docker-feed2imap.git ~/build/docker-feed2imap
+sudo docker build -t feed2imap:$VERSION_FEED2IMAP ~/build/docker-feed2imap
+VERSION_DOVECOT=$VERSION_DOVECOT \
+VERSION_FETCHMAIL=$VERSION_FETCHMAIL \
+VERSION_FEED2IMAP=$VERSION_FEED2IMAP \
  sudo -E bash -c 'docker-compose up -d --force-recreate'
+# --force-recreate is used to recreate container when a file has changed
 # /!\ We cannot remove the secrets files or restarting the container would become impossible.
+rm -rf ~/build

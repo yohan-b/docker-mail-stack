0534749ebeda9bead70817ba19663cd7f20084ea
yohan <783b8c87@scimetis.net>
2020-03-31 15:50:39 +0200

    Using systemd to mount volumes and start containers at boot.

--- create.sh
+++ create.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-test -f ~/secrets.tar.gz.enc || curl -o ~/secrets.tar.gz.enc "https://cloud.scimetis.net/s/${KEY}/download?path=%2F&files=secrets.tar.gz.enc"
+#Absolute path to this script
+SCRIPT=$(readlink -f $0)
+#Absolute path this script is in
+SCRIPTPATH=$(dirname $SCRIPT)
+
+cd $SCRIPTPATH
+
+test -z $KEY && { echo "KEY is not defined."; exit 1; }
+
+if ! test -f ~/secrets.tar.gz.enc
+then
+    curl -o ~/secrets.tar.gz.enc "https://cloud.scimetis.net/s/${KEY}/download?path=%2F&files=secrets.tar.gz.enc"
+    if ! test -f ~/secrets.tar.gz.enc
+    then
+        echo "ERROR: ~/secrets.tar.gz.enc not found, exiting."
+        exit 1
+    fi
+fi
+
 openssl enc -aes-256-cbc -d -in ~/secrets.tar.gz.enc | tar -zxv --strip 2 secrets/docker-mail-stack/fetchmailrc-orange \
 secrets/docker-mail-stack/users secrets/docker-mail-stack/15-lda.conf secrets/docker-mail-stack/feed2imaprc \
-secrets/docker-mail-stack/fetchmailrc-scimetis
+secrets/docker-mail-stack/fetchmailrc-scimetis \
+|| { echo "Could not extract from secrets archive, exiting."; rm -f ~/secrets.tar.gz.enc; exit 1; }
 sudo chown root. fetchmailrc-orange fetchmailrc-scimetis users 15-lda.conf feed2imaprc
 sudo chmod a-r feed2imaprc
-
-VOLUME=mail_data
-sudo mkdir -p /mnt/volumes/${VOLUME}
-if ! mountpoint -q /mnt/volumes/${VOLUME}
-then
-     VOLUME_ID=$(~/env_py3/bin/openstack volume show ${VOLUME} -c id --format value)
-     test -e /dev/disk/by-id/*${VOLUME_ID:0:20} || nova volume-attach $INSTANCE $VOLUME_ID auto
-     sleep 3
-     sudo mount /dev/disk/by-id/*${VOLUME_ID:0:20} /mnt/volumes/${VOLUME} || exit 1
-     mountpoint -q /mnt/volumes/${VOLUME} || { echo "ERROR: could not mount /mnt/volumes/${VOLUME}, exiting."; exit 1; }
-fi
 
 unset VERSION_DOVECOT VERSION_FETCHMAIL VERSION_FEED2IMAP
 VERSION_DOVECOT=$(git ls-remote https://git.scimetis.net/yohan/docker-dovecot.git| head -1 | cut -f 1|cut -c -10)
@@ -33,7 +41,7 @@ sudo docker build -t feed2imap:$VERSION_FEED2IMAP ~/build/docker-feed2imap
 VERSION_DOVECOT=$VERSION_DOVECOT \
 VERSION_FETCHMAIL=$VERSION_FETCHMAIL \
 VERSION_FEED2IMAP=$VERSION_FEED2IMAP \
- sudo -E bash -c 'docker-compose up -d --force-recreate'
+ sudo -E bash -c 'docker-compose up --no-start --force-recreate'
 # --force-recreate is used to recreate container when a file has changed
 # /!\ We cannot remove the secrets files or restarting the container would become impossible.
 rm -rf ~/build
